ada96a7475f690db5f158ec87d0142b19c1ab8c8
RangCloud <mtr008@naver.com>
2024-01-11 15:00:26 +0900

    test

---
+++ test3.java
@@ -0,0 +1,27 @@
+public class test3 {
+    static class A{
+        int a;
+        int b;
+    }
+
+    public static class Main{
+        static void func1(A m){
+            m.a *= 10;
+        }
+
+        static void func2(A m){
+            m.a += m.b;
+        }
+
+        public static void main(String[] args) {
+            A m = new A();
+
+            m.a = 100;
+            func1(m);
+            m.b = m.a;
+            func2(m);
+
+            System.out.println(m.a);
+        }
+    }
+}
